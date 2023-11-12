10f27dd8181d55ace582a541902497292872f8d9
nanamiwaku <sample@.com>
2023-11-11 22:22:30 -0500

    this is first commit

---
+++ labweek09/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development -I</h1>
+        <h2>
+         React JS Programming Week09 Lab exercise
+        </h2>
+        <h3>101371241</h3>
+        <p>Nanami Waku
+           George Brown College, Toronto <br/>
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
